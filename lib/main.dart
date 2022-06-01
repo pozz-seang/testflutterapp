6ed8058573d9f706aa16ad6b1b3071ac44ef9312
SEHacker <69587550+pozz-seang@users.noreply.github.com>
2022-06-01 16:58:42 +0700

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:clipboard/clipboard.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(Home());
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  void _show() {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text("Hi, I am a snack bar!"),
+    ));
+  }
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceWebView: true);
+    } else {
+      throw 'Could not launch $url';
+    }
+ }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xFF00FF95),
-            title: Text(
-              " Hi b sl o",
-              style: TextStyle(color: Color(0xFFFFFFFF)),
-            ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF00FF95),
+          title: Text(
+            "PozzSeang.ONE",
+            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
           ),
-          body: Center(
-            child: TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-              onPressed: () async {
-                FlutterClipboard.copy('HI B SL O').then(( value ) => print('copied'));
-                final snackBar = SnackBar(content: Text("You Copy {HI B SL O}"));
-                ScaffoldMessenger.of(context)..removeCurrentSnackBar()..showSnackBar(snackBar);
-              },
-              child: Text('TEST BUTTON COPY TEXT'),
-            ),
-          )),
+        ),
+        body: Center(
+          child: MaterialButton(
+            color: Color(0xFF00FF95),
+            textColor: Color.fromARGB(255, 0, 0, 0),
+            child: Text('Copy Text Test'),
+            onPressed: () {
+              FlutterClipboard.copy('HI B SL O')
+                  .then((value) => print('copied'));
+              _show;
+              launchURL('pozzseang.one');
+            },
+          ),
+          
+        ),
+      ),
     );
   }
 }
