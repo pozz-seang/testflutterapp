51e62deff4648b9f6c13c3958b5d117a1f955632
SEHacker <69587550+pozz-seang@users.noreply.github.com>
2022-06-01 12:06:51 +0700

    de

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:clipboard/clipboard.dart';
 
-void main(){
+void main() {
   runApp(Home());
 }
 
-
-
-
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFFE600FF),
-          leading: IconButton(icon: Icon(Icons.home,color: Color(0xFFFFFFFF)), onPressed: null,),
-          title: Text("Test By SEHacker", style: TextStyle(color: Color(
-              0xFFFFFFFF)),),
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Color(0xFF00FF95),
+            title: Text(
+              " Hi b sl o",
+              style: TextStyle(color: Color(0xFFFFFFFF)),
+            ),
+          ),
+          body: Center(
+            child: TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () async {
+                FlutterClipboard.copy('HI B SL O').then(( value ) => print('copied'));
+                final snackBar = SnackBar(content: Text("You Copy {HI B SL O}"));
+                ScaffoldMessenger.of(context)..removeCurrentSnackBar()..showSnackBar(snackBar);
+              },
+              child: Text('TEST BUTTON COPY TEXT'),
+            ),
+          )),
     );
   }
 }
-
-
